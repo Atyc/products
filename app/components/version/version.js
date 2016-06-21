bd6e6899ee239cba994e1764de415c5bf8496a1b
Octav <administrator@DevMac.local>
2016-06-21 12:18:23 +0100

    just starting

--- app/components/version/version.js
+++ app/components/version/version.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.version', [
-  'myApp.version.interpolate-filter',
-  'myApp.version.version-directive'
+angular.module('octApp.version', [
+  'octApp.version.interpolate-filter',
+  'octApp.version.version-directive'
 ])
 
 .value('version', '0.1');
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('octApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
